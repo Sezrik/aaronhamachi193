01d2e19dcae12c307b2e46accdd7d2c4f5fe62b3
Sezrik <116929301+Sezrik@users.noreply.github.com>
2022-11-16 14:32:17 -0800

    Add files via upload

---
+++ Lab 2.js
@@ -0,0 +1,32 @@
+let contacts1 = {
+  name: "Maxwell Wright",
+  phone1: "(0191) 719 6495",
+  email1: "Curabitur.egestas.nunc@nonummyac.co.uk"
+  };
+  
+  
+let contacts2 = {
+  name: "Raja Villarreal",
+  phone: "0866 398 2895",
+  email: "posuere.vulputate@sed.com"
+  }; 
+  
+  
+  let contacts3 = {
+  name: "Helen Richards",
+  phone: "0800 1111",
+  email: "libero@convallis.edu"
+  };
+  
+  let contacts4 = {
+  name: "Helen Richards",
+  phone: "0800 1111",
+  email: "risus,Quisque@urna.ca"
+  };
+  
+  
+
+ 
+  
+  console.log(contacts1)
+  console.log(contacts4)
